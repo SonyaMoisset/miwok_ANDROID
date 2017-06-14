ceddf69314a4ae45788edb34af22a4207412257d
smoisset <smoisset@worldremit.com>
2017-06-14 22:48:37 +0100

    Update ColorsActivity.java

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,12 +2,29 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> colors = new ArrayList<>();
+        colors.add(new Word(getResources().getString(R.string.color_red_eng), getResources().getString(R.string.color_red_miwok)));
+        colors.add(new Word(getResources().getString(R.string.color_green_eng), getResources().getString(R.string.color_green_miwok)));
+        colors.add(new Word(getResources().getString(R.string.color_brown_eng), getResources().getString(R.string.color_brown_miwok)));
+        colors.add(new Word(getResources().getString(R.string.color_gray_eng), getResources().getString(R.string.color_gray_miwok)));
+        colors.add(new Word(getResources().getString(R.string.color_black_eng), getResources().getString(R.string.color_black_miwok)));
+        colors.add(new Word(getResources().getString(R.string.color_white_eng), getResources().getString(R.string.color_white_miwok)));
+        colors.add(new Word(getResources().getString(R.string.color_dusty_yellow_eng), getResources().getString(R.string.color_dusty_yellow_miwok)));
+        colors.add(new Word(getResources().getString(R.string.color_mustard_yellow_eng), getResources().getString(R.string.color_mustard_yellow_miwok)));
+
+        WordAdapter wordsAdapter = new WordAdapter(this, colors);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(wordsAdapter);
     }
 }
