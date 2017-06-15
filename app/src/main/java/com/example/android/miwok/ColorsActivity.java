53aeaadcd231a049e20f8d3d54ec46f97cef0237
smoisset <smoisset@worldremit.com>
2017-06-15 09:24:40 +0100

    Update ColorsActivity.java

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -14,14 +14,14 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word(getResources().getString(R.string.color_red_eng), getResources().getString(R.string.color_red_miwok)));
-        words.add(new Word(getResources().getString(R.string.color_green_eng), getResources().getString(R.string.color_green_miwok)));
-        words.add(new Word(getResources().getString(R.string.color_brown_eng), getResources().getString(R.string.color_brown_miwok)));
-        words.add(new Word(getResources().getString(R.string.color_gray_eng), getResources().getString(R.string.color_gray_miwok)));
-        words.add(new Word(getResources().getString(R.string.color_black_eng), getResources().getString(R.string.color_black_miwok)));
-        words.add(new Word(getResources().getString(R.string.color_white_eng), getResources().getString(R.string.color_white_miwok)));
-        words.add(new Word(getResources().getString(R.string.color_dusty_yellow_eng), getResources().getString(R.string.color_dusty_yellow_miwok)));
-        words.add(new Word(getResources().getString(R.string.color_mustard_yellow_eng), getResources().getString(R.string.color_mustard_yellow_miwok)));
+        words.add(new Word(getResources().getString(R.string.color_red_eng), getResources().getString(R.string.color_red_miwok), R.drawable.color_red));
+        words.add(new Word(getResources().getString(R.string.color_green_eng), getResources().getString(R.string.color_green_miwok), R.drawable.color_green));
+        words.add(new Word(getResources().getString(R.string.color_brown_eng), getResources().getString(R.string.color_brown_miwok), R.drawable.color_brown));
+        words.add(new Word(getResources().getString(R.string.color_gray_eng), getResources().getString(R.string.color_gray_miwok), R.drawable.color_gray));
+        words.add(new Word(getResources().getString(R.string.color_black_eng), getResources().getString(R.string.color_black_miwok), R.drawable.color_black));
+        words.add(new Word(getResources().getString(R.string.color_white_eng), getResources().getString(R.string.color_white_miwok), R.drawable.color_white));
+        words.add(new Word(getResources().getString(R.string.color_dusty_yellow_eng), getResources().getString(R.string.color_dusty_yellow_miwok), R.drawable.color_dusty_yellow));
+        words.add(new Word(getResources().getString(R.string.color_mustard_yellow_eng), getResources().getString(R.string.color_mustard_yellow_miwok), R.drawable.color_mustard_yellow));
 
         WordAdapter wordsAdapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
